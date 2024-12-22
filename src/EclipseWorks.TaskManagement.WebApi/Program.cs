24bf570441bb13e49d79f9a7a468fbc01ca76feb
mikerozendo <mikerozendo@gmail.com>
2024-12-22 14:49:08 -0300

    Configure Kestrel for containerized environments

--- src/EclipseWorks.TaskManagement.WebApi/Program.cs
+++ src/EclipseWorks.TaskManagement.WebApi/Program.cs
@@ -1,3 +1,4 @@
+using EclipseWorks.TaskManagement.Application.Responses;
 using EclipseWorks.TaskManagement.Infrastructure;
 using EclipseWorks.TaskManagement.Infrastructure.Repositories;
 using EclipseWorks.TaskManagement.Models;
@@ -11,14 +12,17 @@ builder.Services.AddSwaggerGen();
 var environmentConfiguration = builder.Configuration.Get<EnvironmentConfiguration>();
 ArgumentNullException.ThrowIfNull(environmentConfiguration);
 builder.Services.AddSingleton(environmentConfiguration);
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<ResourceCommandResponse>());
 
 builder.Services.AddScoped<IRepository<Project>, ProjectsRepository>();
 
-builder.WebHost.ConfigureKestrel(serverOptions =>
+if (Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true")
 {
-    serverOptions.ListenAnyIP(8080);
-});
-
+    builder.WebHost.ConfigureKestrel(serverOptions =>
+    {
+        serverOptions.ListenAnyIP(8080);
+    });
+}
 
 var app = builder.Build();
 
