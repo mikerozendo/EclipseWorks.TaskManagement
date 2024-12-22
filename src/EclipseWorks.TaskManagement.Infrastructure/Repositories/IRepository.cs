625c18d224dd91a3548c8a9a824e8571ab421cff
mikerozendo <mikerozendo@gmail.com>
2024-12-22 00:22:05 -0300

    Add MongoDB repository infrastructure + Introduce a generic repository pattern with MongoDB integration, including `ProjectsRepository` and base `Repository` implementation.

---
+++ src/EclipseWorks.TaskManagement.Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using EclipseWorks.TaskManagement.Models;
+
+namespace EclipseWorks.TaskManagement.Infrastructure.Repositories;
+
+public interface IRepository<T> where T : IEntity
+{
+    Task<T?> GetById(Guid id);
+    Task Insert(T record);
+}
---
+++ src/EclipseWorks.TaskManagement.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq.Expressions;
+using EclipseWorks.TaskManagement.Models;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+namespace EclipseWorks.TaskManagement.Infrastructure.Repositories;
+
+public class Repository<T> where T : IEntity
+{
+    protected readonly IMongoCollection<T> Collection;
+
+    protected Repository(string connectionString, string dataBaseName, string collectionName)
+    {
+        var mongoClient = new MongoClient(connectionString);
+        var database = mongoClient.GetDatabase(dataBaseName);
+        Collection = database.GetCollection<T>(collectionName);
+    }
+
+    public async Task<IEnumerable<T>> Get(Expression<Func<T, bool>> predicate)
+    {
+        var filter = Builders<T>.Filter.Where(predicate);
+        return await Collection.Find(filter).ToListAsync();
+    }
+
+    public async Task<T?> GetById(Guid id)
+    {
+        return await Collection
+            .AsQueryable()
+            .FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task Insert(T record)
+    {
+        await Collection.InsertOneAsync(record);
+    }
+}
